fea57911eb3d33893223e1b737440cb317c901b1
ram4ik <ramill_ibragimov@icloud.com>
2024-12-19 09:21:01 +0200

    Initial Commit

---
+++ ImplementingCustomPopUpInSwiftUIUsingViewModifier/ImplementingCustomPopUpInSwiftUIUsingViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementingCustomPopUpInSwiftUIUsingViewModifierApp.swift
+//  ImplementingCustomPopUpInSwiftUIUsingViewModifier
+//
+//  Created by admin on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ImplementingCustomPopUpInSwiftUIUsingViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
